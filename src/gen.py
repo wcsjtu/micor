637aac697f008bbd567caa80e80843add916b94f
王超 <hzwangchao3@corp.netease.com>
2018-12-24 22:47:12 +0800

    add ioloop

--- src/gen.py
+++ src/gen.py
@@ -1,129 +1,75 @@
-#coding:utf8
-import time
+﻿import sys
+from functools import wraps
 from types import GeneratorType
-from functools import partial, wraps
 
-class EventList:
 
-    _instance = None
+class Future(object):
 
-    @classmethod
-    def current(cls):
-        if not cls._instance:
-            cls._instance = cls()
-        return cls._instance
+    __slots__ = ["_callbacks", "_exc_info", "_result", "_done"]
 
     def __init__(self):
-        self._ready = []
-        self._not_ready = []
+        self._callbacks = list()
+        self._exc_info = None
+        self._result = None
+        self._done = False
 
-    def add_callback(self, cb, *args, **kw):
-        fn = partial(cb, *args, **kw)
-        self._ready.append(fn)
+    def add_done_callback(self, callback):
+        self._callbacks.append(callback)
 
-    def add_timer(self, timer):
-        self._not_ready.append(timer)
+    def result(self):
+        return self._result
 
-    def _check_not_ready(self):
-        now = time.time()
-        notready = []
-        while self._not_ready:
-            timer = self._not_ready.pop()
-            if timer.due <= now:
-                self._ready.append(timer.callback)
-            else:
-                notready.append(timer)
-        self._not_ready = notready
+    def set_result(self, result):
+        self._result = result
+        self.set_done()
 
-    def run(self):
-        while (self._ready or self._not_ready):
-            self._check_not_ready()
-            if self._ready:
-                cb = self._ready.pop()
-                cb()
-
-    
-class Future:
-
-    def __init__(self):
-        self._done = False      # 状态, 表示是否结束了
-        self._callbacks = []    
-        self._result = None     # 结果
-
-    def add_done_callback(self, cb):
-        self._callbacks.append(cb)
-
-    def set_result(self, r):
-        self._result = r
+    def set_exc_info(self, exc_info):
+        self._exc_info = exc_info
         self.set_done()
 
     def set_done(self):
         self._done = True
         for cb in self._callbacks:
-            cb(self)
+            try:
+                cb(self)
+            except Exception:
+                print('Exception in callback %r for %r' % (cb, self))
+        self._callbacks = None
 
-
-class Timer:
-
-    def __init__(self, due, callback):
-        self.due = due
-        self.callback = callback
-        self.register()
-
-    def __lt__(self, other):
-        return self.due < other.due
-
-    def __le__(self, other):
-        return self.due <= other.due
-
-    def register(self):
-        EventList.current().add_timer(self)
-
-def sleep(timeout):
-    future = Future()
-    due = time.time() + timeout
-    Timer(due, lambda: future.set_result(None))
-    return future
 
 def _next(gen, future, value=None):
     try:
-        val = value if not value else value._result
-        fut = gen.send(val)
-        fut.add_done_callback(lambda v: _next(gen, future, v))
-    except StopIteration as exc:
-        future.set_result(exc.value)
-
+        if not value:
+            fut = gen.send(value)
+        else:
+            if value._exc_info:
+                fut = gen.throw(value._exc_info)
+            else:
+                fut = gen.send(value._result)
+        fut.add_done_callback(lambda value: _next(gen, future, value))
+    except StopIteration as e:
+        future.set_result(e.value)
+    except Exception:
+        future.set_exc_info(sys.exc_info())
+        import traceback
+        traceback.print_tb(sys.exc_info()[2])
 
 def coroutine(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         future = Future()
-        gen = func(*args, **kwargs)
-        if isinstance(gen, GeneratorType):
-            _next(gen, future)
+        try:
+            gen = func(*args, **kwargs)
+        except Exception:
+            future.set_exc_info(sys.exc_info())
             return future
+        else:
+            if isinstance(gen, GeneratorType):
+                _next(gen, future)
+                return future
         future.set_result(gen)
         return future
+
     return wrapper
 
 
-if __name__ == "__main__":
-    
-    events = EventList.current()
-
-    case1 = [1, 2, 3, 4, 5, 6]
-    case2 = ["a", "b", "c", "d", "e", "f", "g"]
-
-    @coroutine
-    def show(case):
-        for v in case:
-            future = Future()
-            events.add_callback(lambda: future.set_result(None))
-            print(v)
-            yield sleep(1)
-
-    show(case1)
-    show(case2)
-
-    events.run()
-    
---
+++ src/ioloop.py
@@ -0,0 +1,173 @@
+﻿import select
+from time import time
+from functools import partial
+from collections import defaultdict, deque
+from gen import Future
+
+class SelectImpl:
+
+    def __init__(self):
+        self._r_list = set()
+        self._w_list = set()
+        self._x_list = set()
+
+    def poll(self, timeout):
+        r, w, x = select.select(self._r_list, self._w_list, self._x_list, timeout)
+        results = defaultdict(lambda: 0x00)
+        for p in [(r, IOLoop.READ), (w, IOLoop.WRITE), (x, IOLoop.ERROR)]:
+            for fd in p[0]:
+                results[fd] |= p[1]
+        return results.items()
+
+    def register(self, fd, mode: int):
+        if mode & IOLoop.READ:
+            self._r_list.add(fd)
+        if mode & IOLoop.WRITE:
+            self._w_list.add(fd)
+        if mode & IOLoop.ERROR:
+            self._x_list.add(fd)
+
+    def unregister(self, fd):
+        if fd in self._r_list:
+            self._r_list.remove(fd)
+        if fd in self._w_list:
+            self._w_list.remove(fd)
+        if fd in self._x_list:
+            self._x_list.remove(fd)
+
+    def modify(self, fd, mode):
+        self.unregister(fd)
+        self.register(fd, mode)
+
+    def close(self):
+        pass
+
+
+try:
+    from select import epoll as PollImpl
+except:
+    PollImpl = SelectImpl
+
+
+class IOLoop:
+
+    READ = _EPOLLIN = 0x001
+    WRITE = _EPOLLOUT = 0x004
+    ERROR = 0x008 | 0x010
+
+    TIMEOUT = 10
+
+    DEFAULT_HANDLER = (None, None)
+
+    _instance = None
+
+    @classmethod
+    def current(cls):
+        if not cls._instance:
+            cls._instance = cls()
+        return cls._instance
+
+    def __init__(self):
+        self._stop = False
+        self._ready = deque()
+        self._timer = list()
+        self._fds = dict()
+        self._impl = PollImpl()
+
+    def add_callback(self, callback, *args, **kwargs):
+        fn = partial(callback, *args, **kwargs)
+        self._ready.append(fn)
+
+    def add_timer(self, timer):
+        self._timer.append(timer)
+
+    def add_future(self, future, callback):
+
+        def cb(fut):
+            return self.add_callback(callback, fut)
+
+        future.add_done_callback(cb)
+
+    def register(self, sock, mode, handler):
+        fd = sock.fileno()
+        self._fds[fd] = (sock, handler)
+        self._impl.register(fd, mode)
+
+    def unregister(self, sock):
+        fd = sock.fileno()
+        self._fds.pop(fd, None)
+        try:
+            self._impl.unregister(fd)
+        except Exception as err:
+            print("Error deleting fd from IOLoop: ", err)
+
+    def mod_register(self, sock, events):
+        fd = sock.fileno()
+        self._impl.modify(fd, events | self.ERROR)
+
+    def stop(self):
+        self._stop = True
+        self._ready = None
+        self._timer = list()
+        self._fds = dict()
+        self._impl.close()
+
+    def run_ready(self):
+        while self._ready:
+            cb = self._ready.popleft()
+            cb()
+
+    def run_due_timer(self):
+        now = time()
+        todo = []
+        while self._timer:
+            timer = self._timer.pop()
+            if timer.due <= now:
+                timer.callback()
+            else:
+                todo.append(timer)
+        self._timer = todo
+
+    def run(self):
+        while not self._stop:
+            if self._ready:
+                self.run_ready()
+            self.run_due_timer()
+            timeout = self.TIMEOUT
+            if self._ready:
+                timeout = 0
+            elif self._timer:
+               self._timer.sort()
+               delta = self._timer[0].due - time()
+               timeout = max(0, delta)
+            events = self._impl.poll(timeout=timeout)
+            for fd, event in events:
+                sock, handler = self._fds.get(fd, self.DEFAULT_HANDLER)
+                if not sock:
+                    continue
+                handler(sock, fd, event)
+        return None
+
+
+class Timer(object):
+
+    def __init__(self, due: float, callback):
+        self.due = due
+        self.callback = callback
+        self.register()
+
+    def __lt__(self, other):
+        return self.due < other.due
+
+    def __le__(self, other):
+        return self.due <= other.due
+
+    def register(self):
+        IOLoop.current().add_timer(self)
+
+
+def sleep(timeout):
+    future = Future()
+    due = time() + timeout
+    Timer(due, lambda: future.set_result(None))
+    return future
