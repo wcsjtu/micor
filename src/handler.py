09ff2b51726019b17df7a789b1b3a86f5f07d666
wcsjtu <wcsjtu@163.com>
2019-08-20 21:21:21 +0800

    add icmp handler

--- src/handler.py
+++ src/handler.py
@@ -449,3 +449,67 @@ class UDPClient(BaseHandler):
     def _read(self):
         self._rfut = Future()
         return self._rfut
+
+
+class ICMPClient(BaseHandler):
+
+    def __init__(self, loop=None):
+        if not loop:
+            loop = IOLoop.current()
+        super().__init__(loop)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        self._rfut = None
+        self.register()
+
+    @coroutine
+    def ping(self, dst, timeout=2):
+
+        if not ip_type(dst):
+            future =  resolver.getaddrinfo(dst, 0, proto=socket.IPPROTO_ICMP)
+            start = time.time()
+            timer = self._loop.add_calllater(timeout,
+                    lambda: future.cancel((errors.TimeoutError, None, None))
+                )
+            res = yield future
+            self._loop.remove_timer(timer)
+            timeout -= (time.time() - start)
+            family, tp, proto, cn, sa = res[0]
+        else:
+            sa = (dst, 0)
+
+        ts = struct.pack("!Q", int(time.time()*1000))
+        pkg = cares.build_ping_pkg(ts, 1, 1)
+        self._sock.sendto(pkg, sa)
+        
+        future = self.recvfrom()
+        timer = self._loop.add_calllater(
+            timeout,lambda: future.cancel((errors.TimeoutError, None, None)))
+        res, svr = yield future
+        self._loop.remove_timer(timer)
+
+        frame = cares.parse_ping_pkg(res)
+        rtt = time.time()*1000 - struct.unpack("!Q", frame.data)[0]
+        print("8 bytes from %s: icmp_seq=1 ttl=%d time=%fms" % (svr[0], frame.ip_ttl, rtt))
+
+
+    def handle(self, sock, fd, events):
+        if events & self._loop.ERROR:
+            print("icmp socket eroor")
+            self.close()
+        if events& self._loop.READ:
+            self.on_read()
+
+    def on_read(self):
+        res = self._sock.recvfrom(65535)
+        if self._rfut:
+            self._rfut.set_result(res)
+
+    def close(self):
+        self._sock.close()
+        self._rfut = None
+        self._loop.unregister(self._sock)
+
+    def recvfrom(self):
+        future = Future()
+        self._rfut = future
+        return future
